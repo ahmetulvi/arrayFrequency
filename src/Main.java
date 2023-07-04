3c6afeecc748244d9d4a09f0042675e0f852faf3
ahmetulvi <47754871+ahmetulvi@users.noreply.github.com>
2023-07-05 00:16:23 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] arr={2,2,3,3,5,1,2,123,523,523,123,88,99,1};
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for (int i=0;i< arr.length;i++){
+            int counter=0;
+            boolean isCount=false;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            for (int j=0;j<i;j++){
+                if (arr[j]==arr[i]){
+                    isCount=true;
+                    break;
+                }
+
+            }
+            if (!isCount){
+                for (int k=0;k< arr.length;k++){
+                    if (arr[i]==arr[k])
+                        counter++;
+                }
+            }
+            if (counter>=2)
+                System.out.println(arr[i] +" repeated "+counter+" times.");
         }
+
     }
 }
